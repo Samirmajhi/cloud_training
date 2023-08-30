1eb9a89b0452ee33ebfbdc15d797fdb9da8e763b
samir kumar majhi <107356531+Samirmajhi@users.noreply.github.com>
2023-08-30 22:41:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,32 @@
 const express = require('express');
-
 const app = express();
 
 const mysqlutils = require('./mysqlutils');
-const dynamodbutils = require('./dynamodbutils');
 const redisutils = require('./redisutils');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-    });
+});
 
 app.get('/students', async (req, res) => {
     try {
-        let redisCacheData = await redisutils.getFromRedis();
+        let redisCacheData = await redisutils.getFromRedis('students');
         if (redisCacheData) {
             console.log('Data from Redis Cache');
-            res.send(JSON.parse(redisCacheData));
+            const jsonData = JSON.parse(redisCacheData);
+            res.send(`<h1>Student Data from Redis</h1><pre>${JSON.stringify(jsonData, null, 2)}</pre>`);
         } else {
-            console.log('Data from MySQL');
-            let response  = await mysqlutils.getStudentsdatafromRDS();
+            console.log('Data from MySQL RDS');
+            let response = await mysqlutils.getStudentsDataFromRDS();
             await redisutils.setToRedisWithExpiry('students', JSON.stringify(response), 20);
-            res.send(response);
+            res.send(`<h1>Student Data from MySQL RDS</h1><pre>${JSON.stringify(response, null, 2)}</pre>`);
         }
     } catch (error) {
         console.log(`Error: ${error}`);
-        // 400 response code
-        res.sendStatus(400);
-    }
-});
-
-app.get('/dynamodb/students', async (req, res) => {
-    try {
-        let response  = await dynamodbutils.getDataFromDynamoDB();
-        res.send(response);
-    } catch (error) {
-        console.log(`Error: ${error}`);
-        // 400 response code
-        res.sendStatus(400);
+        res.status(500).send('<h1>Error</h1><p>Internal Server Error</p>'); // HTML error response
     }
 });
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
-    });
-    
+});
